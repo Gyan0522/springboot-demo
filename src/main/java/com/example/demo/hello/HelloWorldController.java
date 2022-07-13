2ca914302a78502d13c5c3b05e345a44450a996b
Gyan Prakash <gprakas2@in.ibm.com>
2022-07-13 20:02:50 +0530

    Hello World Base

---
+++ src/main/java/com/example/demo/hello/HelloWorldController.java
@@ -0,0 +1,21 @@
+package com.example.demo.hello;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController{
+
+@RequestMapping(method = RequestMethod.GET, path= "/helloworld1")
+public String helloworld(){
+    return "hello world";
+}
+@GetMapping("/helloworld-bean")
+public UserDetails helloWorldBean(){
+
+    return new UserDetails("gyan","prakash","Bangalore");
+}
+}
+
